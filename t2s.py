e1b283d8e3fb08a99aa693be74222095337afa44
Buckyron <harivicky2020@gmail.com>
2020-12-01 12:21:11 +0530

    Added GPT2 story generator

--- t2s.py
+++ t2s.py
@@ -2,11 +2,9 @@ import os
 import time
 import speech_recognition as sr
 from gtts import gTTS 
-
-
-import pyttsx3
 import PyPDF2
 import pikepdf
+from transformers import pipeline
 
 # Convert text to speech and save it as audiofile.
 def speak(text):
@@ -14,22 +12,37 @@ def speak(text):
     filename = "audioFile.mp3"
     tts.save(filename)
 
-# Decrypt the pdf and save it.
-pdf = pikepdf.open('demo_story.pdf',password='')
-pdf.save('demo_story_decrypted.pdf')
+# Generate story using GPT2 model.
+story_gen = pipeline("text-generation", "pranavpsv/gpt2-genre-story-generator")
+input_prompt = "<BOS> <horror> In the middle of a "
+story = story_gen(input_prompt, max_length=200, do_sample=True,
+               repetition_penalty=1.1, temperature=1.2, 
+               top_p=0.95, top_k=50)
 
-# Read every page and create a text file.
-with open('demo_story_decrypted.pdf','rb') as pdf_file, open('sample.txt', 'w') as text_file:
-    read_pdf = PyPDF2.PdfFileReader(pdf_file)
-    number_of_pages = read_pdf.getNumPages()
-    for page_number in range(number_of_pages):   # use xrange in Py2
-        page = read_pdf.getPage(page_number)
-        page_content = page.extractText()
-        text_file.write(page_content)
+print(story[0]["generated_text"])
 
-# Read the text from the text file and save it in a variable.
-with open('sample.txt', 'r') as file:
-    text = file.read().replace('\n', '')
+storyString = ''.join(story[0]["generated_text"])
+
+with open('sample.txt', 'w') as text_file:
+    text_file.write(str(storyString))
+
+
+# # Decrypt the pdf and save it.
+# pdf = pikepdf.open('demo_story.pdf',password='')
+# pdf.save('demo_story_decrypted.pdf')
+
+# # Read every page and create a text file.
+# with open('demo_story_decrypted.pdf','rb') as pdf_file, open('sample.txt', 'w') as text_file:
+#     read_pdf = PyPDF2.PdfFileReader(pdf_file)
+#     number_of_pages = read_pdf.getNumPages()
+#     for page_number in range(number_of_pages):   # use xrange in Py2
+#         page = read_pdf.getPage(page_number)
+#         page_content = page.extractText()
+#         text_file.write(page_content)
+
+# # Read the text from the text file and save it in a variable.
+# with open('sample.txt', 'r') as file:
+#     text = file.read().replace('\n', '')
 
 # Call text to speech.
-speak(text)
+speak(storyString)
